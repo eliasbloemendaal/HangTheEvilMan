02f5126a10c2acb49dbdc5295ce7dca64ca61426
eliasbloemendaal <eliasbloemendaal@gmail.com>
2015-11-24 15:17:32 +0100

    Initial Commit

---
+++ HangTheEvilManTests/HangTheEvilManTests.swift
@@ -0,0 +1,36 @@
+//
+//  HangTheEvilManTests.swift
+//  HangTheEvilManTests
+//
+//  Created by elias bloemendaal on 24/11/2015.
+//  Copyright © 2015 elias bloemendaal. All rights reserved.
+//
+
+import XCTest
+@testable import HangTheEvilMan
+
+class HangTheEvilManTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
